43ce158fd3f008321ba4ebd62e1fd6617932d5f2
Alexandre Binninger <alexandre.binninger@inf.ethz.ch>
2023-12-31 17:54:24 +0100

    Slight modifications

--- assets/scripts/hobbies.js
+++ assets/scripts/hobbies.js
@@ -5,20 +5,45 @@ const hobbies = [
     "philosophy",
     "video games",
     "digital art",
-    "A.I.",
+    "Gen A.I.",
     "languages"
 ];
 const element = document.querySelector(".hobbies");
 let currentHobbyIndex = 0;
 let currentText = "";
+let fontSizeRef = 0;
+
+function DecreaseFontSize() {
+    const container = document.querySelector('.hobbies-section');
+    const fullText = document.querySelector('.hobbies-fulltext');
+    let fontSize = parseFloat(window.getComputedStyle(fullText).fontSize);
+    // Reduce font size until the full text (prefix + hobby) fits
+    while (fullText.offsetWidth > container.offsetWidth && fontSize > 0) {
+        fontSize -= 0.1; // Decrease step can be smaller for smoother adjustment
+        fullText.style.fontSize = fontSize + 'px';
+    }
+}
+
+
+function IncreaseFontSize() {
+    const container = document.querySelector('.hobbies-section');
+    const fullText = document.querySelector('.hobbies-fulltext');
+    let fontSize = parseFloat(window.getComputedStyle(fullText).fontSize);
+    // Reduce font size until the full text (prefix + hobby) fits
+    while (fullText.offsetWidth < container.offsetWidth && fontSize < fontSizeRef) {
+        fontSize += 0.1; // Decrease step can be smaller for smoother adjustment
+        fullText.style.fontSize = fontSize + 'px';
+    }
+}
 
 function typeHobby() {
     if (currentText.length < hobbies[currentHobbyIndex].length) {
         currentText += hobbies[currentHobbyIndex].charAt(currentText.length);
         element.textContent = currentText;
-        setTimeout(typeHobby, 150); // Adjust the typing speed here
+        DecreaseFontSize(); // Call font size adjustment here
+        setTimeout(typeHobby, 150);
     } else {
-        setTimeout(deleteHobby, 1000); // Adjust the delay before deleting here
+        setTimeout(deleteHobby, 1000);
     }
 }
 
@@ -26,13 +51,18 @@ function deleteHobby() {
     if (currentText.length > 0) {
         currentText = currentText.slice(0, -1);
         element.textContent = currentText;
-        setTimeout(deleteHobby, 100); // Adjust the deleting speed here
+        IncreaseFontSize(); // Also call it here
+        setTimeout(deleteHobby, 100);
     } else {
         currentHobbyIndex = (currentHobbyIndex + 1) % hobbies.length;
-        setTimeout(typeHobby, 500); // Adjust the delay before typing the next hobby here
+        setTimeout(typeHobby, 500);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    window.onload = () => {
+        const fullText = document.querySelector('.hobbies-fulltext');
+        fontSizeRef = parseFloat(window.getComputedStyle(fullText).fontSize);
+    }
     typeHobby();
 });
